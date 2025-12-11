de3303360950904ef70c6b95156e0c905fdb5a3d
Timothy Craig <tcraig@frameshift.io>
2025-12-11 12:00:19 -0500

    Adds lorem ipsum generator

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -38,3 +38,8 @@ alias ..="cd .."
 
 # Get public ip address
 alias getpubip="dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tee /dev/tty | pbcopy"
+
+# Lorem ipsum generator
+lorem() {
+    curl -s http://metaphorpsum.com/sentences/${1-3} | pbcopy;
+}
